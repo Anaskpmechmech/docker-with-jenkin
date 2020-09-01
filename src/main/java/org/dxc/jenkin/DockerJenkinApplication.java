d05e28ab2bd811348e88e4ae605675f973b435af
- <mohammedanaskp@dxc.com>
2020-09-01 13:15:37 +0530

    first commit

---
+++ src/main/java/org/dxc/jenkin/DockerJenkinApplication.java
@@ -0,0 +1,13 @@
+package org.dxc.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/dxc/jenkin/model/Product.java
@@ -0,0 +1,18 @@
+package org.dxc.jenkin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	private String productName;
+	private String productCategory;
+	private float price;
+	private int quantity;
+
+}
